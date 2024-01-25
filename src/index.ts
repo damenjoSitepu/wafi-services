8d06a0db14a8213b279bbd38914bcc501970a84b
Damenjo Sitepu <damenstory@gmail.com>
2024-01-25 07:38:16 +0700

    Fix(index.ts): add try catch block when calling app class

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@ import App from "./app";
 
 const PORT: number = Number(process.env.PORT) || 8080;
 
-const app = new App(PORT);
-app.listen();
+try {
+  const app = new App(PORT);
+  app.listen();
+} catch (e: any) {
+  console.log(e.message);
+}
+
 
 
