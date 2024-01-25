4a90f28a368a49eb5d394d63a438c5534a4b7975
Damenjo Sitepu <damenstory@gmail.com>
2024-01-25 08:16:34 +0700

    Chore(package.json): tsc watcher compilation in dev mode

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,7 @@ import App from "./app";
 const PORT: number = Number(process.env.PORT) || 8080;
 
 try {
-  const app = new App(PORT);
-  app.listen();
+  (new App(PORT)).listen();
 } catch (e: any) {
   console.log(e.message);
 }
